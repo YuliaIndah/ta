f2fa2c269757ddcf5d7e2c2b456b584022bec687
Gilas <wiizardchan@gmail.com>
2018-02-26 17:39:17 +0700

    embul

--- application/views/LoginV.php
+++ application/views/LoginV.php
@@ -1,92 +1,83 @@
-<!DOCTYPE html>  
-<html lang="en">  
-<head>  
- <title>Masuk</title>  
- <meta charset="utf-8">  
- <meta http-equiv="X-UA-Compatible" content="IE=edge">  
- <meta name="viewport" content="width=device-width, initial-scale=1">  
- <!-- Latest compiled and minified CSS -->  
- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >  
- <style type="text/css">  
- .form-box{  
-   max-width: 500px;  
-   position: relative;  
-   margin: 5% auto;  
- }  
-</style>  
-</head>  
-<body>  
- <div class="wrapper">  
-  <div class="container">  
-   <div class="row"> 
-    <div class="col-6 col-md-4"></div>
-    <div class="col-6 col-md-4"> 
-      <div class="form-box">  
-       <div class="panel panel-primary"> 
-        <div class="panel panel-header">
-          <center>
-            <img src="<?php echo base_url();?>assets/image/logo/logo-ugm.png" style="height: 40%; width: 40%;">
-            <!-- <h3>Silahkan Masuk : </h3> -->
-            <br>
-            <br>
-          </center>
-        </div>
-        <div class="panel-body">  
-         <div class="row">  
-           <div class="col-sm-12">  
-             <?php echo $this->session->flashdata('msg'); ?>  
-           </div>  
-         </div>  
-         <form action="<?php echo base_url(); ?>user/registration" method="post">  
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
 
-           <?php 
-           $data=$this->session->flashdata('sukses');
-           if($data!=""){ ?>
-           <div class="alert alert-success"><strong>Sukses! </strong> <?=$data;?></div>
-           <?php } ?>
-           <?php 
-           $data2=$this->session->flashdata('error');
-           if($data2!=""){ ?>
-           <div class="alert alert-danger"><strong> Error! </strong> <?=$data2;?></div>
-           <?php } ?>
-          
-           <div class="form-group">  
-            <label class="control-label" for="pswd">Email</label>  
-            <div>  
-              <input type="email" class="form-control" id="email" name="email" placeholder="Email" required="">  
-              <span class="text-danger"><?php echo form_error('email'); ?></span>  
-            </div>  
-          </div>  
-          <div class="form-group">  
-            <label class="control-label" for="sandi">Sandi</label>  
-            <div>  
-              <input type="password" class="form-control" id="password" name="password" placeholder="Password" required="">  
-              <span class="text-danger"><?php echo form_error('password'); ?></span>  
-            </div>  
-          </div>   
-          <div class="form-group">   
-            <div class="row">  
-              <div class="col-sm-offset col-sm-3 btn-submit">  
-               <button type="submit" class="btn btn-success">Masuk</button>  
-             </div>  
-           </div>  
-         </div>  
-       </form>
-     </div>  
-     <div class="panel-footer text-center">  
-      <a href="#"> Lupa Sandi ? </a>
-      <a> | </a>
-      <a href="#"> Daftar </a>
-    </div>  
-  </div>  
-</div> 
-</div> 
-<div class="col-6 col-md-4"></div>
-</div>  
-</div>  
-</div>  
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->  
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>  
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>    
-</body>  
-</html>  
+    <div class="container">    
+        <div id="loginbox" style="margin-top:22px;" class="mainbox col-md-4 col-md-offset-4 col-sm-8 col-sm-offset-2">        
+            <center>
+                <img src="<?php echo base_url();?>assets/image/logo/logo-ugm.png" style="height: 25%; width: 45%;">
+                <!-- <h3>Silahkan Masuk : </h3> -->
+                <br>
+                <br>
+            </center>
+            <div class="panel panel-info" >
+                    <div class="panel-heading">
+                        <div class="panel-title">Silahkan Masuk Disini</div>                     
+                    </div>     
+
+                    <div style="padding-top:20px" class="panel-body" >
+
+                        <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
+                            
+                        <form action="<?php echo site_url('LoginC/signin'); ?>" method="post">
+
+                        <?php 
+                        $data=$this->session->flashdata('sukses');
+                        if($data!=""){ ?>
+                        <div class="alert alert-success"><strong>Sukses! </strong> <?=$data;?></div>
+                        <?php } ?>
+                        <?php 
+                        $data2=$this->session->flashdata('error');
+                        if($data2!=""){ ?>
+                        <div class="alert alert-danger"><strong> Error! </strong> <?=$data2;?></div>
+                        <?php } ?>
+
+                                    
+                            <div style="margin-bottom: 20px" class="input-group">
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
+                                <input type="text" name="email" class="form-email form-control" placeholder="email" autofocus required oninvalid="this.setCustomValidity('email tidak boleh kosong')" oninput="setCustomValidity('')">                                       
+                            </div>
+                                
+                            <div style="margin-bottom: 20px" class="input-group">
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                                <input type="password" name="password" class="form-password form-control" placeholder="Sandi" required oninvalid="this.setCustomValidity('Sandi tidak boleh kosong')" oninput="setCustomValidity('')">
+                            </div>
+
+                            <div style="margin-bottom: 20px; margin-left: 40px;" class="input-group" >
+                                <?=$cap_img?>
+                            </div>
+
+                            <div style="margin-bottom: 20px" class="input-group">
+                               <span class="input-group-addon"><i class="glyphicon glyphicon-pencil"></i></span>
+                               <input type="text" name="captcha" class="form-email form-control" placeholder="Masukkan Captcha" required oninvalid="this.setCustomValivdity('Captcha tidak boleh kosong')" oninput="setCustomValidity('')">
+                            </div>
+                                    
+
+                                <div style="margin-top:10px" class="form-group">
+                                    <!-- Button -->
+
+                                    <div class="col-sm-12 controls">
+                                       <button type="submit" class="btn btn-primary">Masuk</button>
+                                      <a style="float: right; " href="#">Lupa Password</a>
+                                    </div>
+                                </div>
+
+
+                                <div class="form-group">
+                                    <div class="col-md-12 control">
+                                        <div style="border-top: 1px solid#888; margin-top:10px; margin-bottom: 10px; font-size:85%" >
+                                        </div>
+                                        <div>
+                                          Belum punya akun ?
+                                        <a href="<?php echo site_url('LoginC/daftar')?>">Daftar</a>
+                                        </div>
+                                    </div>
+                                </div>    
+                            </form>     
+                        </div>                     
+                    </div>  
+        </div>
+       
+    </div>
+    
